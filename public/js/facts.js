b2b7039fcbe6c91d23f56d999bea18596a3d00e5
kalmbach <kalmbach@gmail.com>
2014-04-07 00:16:44 -0300

    Second iteration, added more persons

--- public/js/facts.js
+++ public/js/facts.js
@@ -1,27 +1,52 @@
 (function($) {
   var facts = [];
 
-  function get_next_fact() {
-    var currentFact = Number($("div#fact").data("fact"));
-    var fact = currentFact;
+  function getNextPerson() {
+    var currentPerson = $("div#persons span:visible").attr('id');
+    var person = currentPerson;
+    var persons = [
+      "anthcourtney", "brunomattarollo", "daniellehickie", "jamesgoodridge",
+      "jonjames", "jorgekalmbach", "lisabritt", "robertwhite", "vikkifurse"];
 
-    while(currentFact == fact) {
-      fact = Math.floor(Math.random() * facts.length);
+    while(currentPerson == person) {
+      person = persons[Math.floor(Math.random() * persons.length)];
     }
 
-    $("div#fact").hide().html(facts[fact]).data("fact", fact).fadeIn(1500);
+    $("#" + currentPerson).fadeOut(1000, function(){
+      $("#" + person).fadeIn(1000);
+    });
+
+    return person;
+  }
+
+  function getNextFact() {
+    var currentFactIndex = Number($("div#fact").data("fact"));
+    var person = $("#" + getNextPerson()).data("name");
+    var factIndex = currentFactIndex;
+    var fact = "";
+
+    while(currentFactIndex == factIndex) {
+      factIndex = Math.floor(Math.random() * facts.length);
+    }
+
+    fact = facts[factIndex].replace("{person}", person);
+    $("div#fact").fadeOut(1000, function(){
+      $("div#fact").html(fact).data("fact", factIndex).fadeIn(1000);
+    });
+
+    return fact;
   }
 
   $(function() {
     $.get("/data/facts.txt",function(data) {
       facts = data.split("\n");
       facts.pop();
-      get_next_fact();
+      getNextFact();
     });
 
     $("div#next a").bind("click", function(event) {
       event.preventDefault();
-      get_next_fact();
+      getNextFact();
     });
   });
 })(jQuery);
