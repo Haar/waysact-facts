ddef273e69ca5eab74728b3459c72e8e1bfc9205
kalmbach <kalmbach@gmail.com>
2014-04-10 00:14:25 -0300

    Fix some visual bugs (showing more than one person)
    - Prevent multiple clicks triggering multiple times the binded function.
    - Showing persons/facts in a more controlled way. Repeating the full list but
      showing each person/fact only once in each repetition.

--- public/js/facts.js
+++ public/js/facts.js
@@ -1,37 +1,47 @@
 (function($) {
-  var facts = [];
+  facts = [];
+  persons = [
+    "anthcourtney", "brunomattarollo", "daniellehickie", "jamesgoodridge",
+    "jonjames", "jorgekalmbach", "lisabritt", "robertwhite", "vikkifurse"];
+
+  shown_facts = [];
+  shown_persons = [];
+
+  working = false;
 
   function getNextPerson() {
-    var currentPerson = $("div#persons span:visible").attr('id');
-    var person = currentPerson;
-    var persons = [
-      "anthcourtney", "brunomattarollo", "daniellehickie", "jamesgoodridge",
-      "jonjames", "jorgekalmbach", "lisabritt", "robertwhite", "vikkifurse"];
+    var person;
 
-    while(currentPerson == person) {
-      person = persons[Math.floor(Math.random() * persons.length)];
+    if (persons.length == 0) {
+      while (shown_persons.length > 0) {
+        persons.push(shown_persons.pop())
+      }
     }
 
-    $("#" + currentPerson).fadeOut(1000, function(){
-      $("#" + person).fadeIn(1000);
-    });
+    person = persons.splice(Math.floor(Math.random() * persons.length), 1).pop();
+    shown_persons.push(person)
 
     return person;
   }
 
   function getNextFact() {
-    var currentFactIndex = Number($("div#fact").data("fact"));
-    var person = $("#" + getNextPerson()).data("name");
-    var factIndex = currentFactIndex;
-    var fact = "";
+    var name, fact;
 
-    while(currentFactIndex == factIndex) {
-      factIndex = Math.floor(Math.random() * facts.length);
+    if (facts.length == 0) {
+      while (shown_facts.length > 0) {
+        facts.push(shown_facts.pop());
+      }
     }
 
-    fact = facts[factIndex].replace(/\{person\}/g, person);
-    $("div#fact").fadeOut(1000, function(){
-      $("div#fact").html(fact).data("fact", factIndex).fadeIn(1000);
+    person = getNextPerson();
+    name = $("#" + person).data("name");
+    fact = facts.splice(Math.floor(Math.random() * facts.length), 1).pop();
+    shown_facts.push(fact);
+    fact = fact.replace(/\{person\}/g, name);
+
+    $("div#fact, #persons > span:visible").fadeOut(1000, function(){
+      $("#" + person).fadeIn(1000);
+      $("div#fact").html(fact).fadeIn(1000);
     });
 
     return fact;
